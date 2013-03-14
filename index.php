7f18cb882975bf753db3e0efa400c1b992ddc82d
Iván Eixarch <miao.org@gmail.com>
2013-03-14 23:05:55 +0100

    index de prueba

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="title" CONTENT="PAH: Gestión de Afectados" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<style>
+html,body{height:100%;width:100%;text-align:center;}
+body{background:#fff url(logoPAH.jpg) no-repeat fixed center center;}
+h1{color:#900;}
+</style>
+</head>
+<body>
+<h1>Próximamente</h1>
+</body>
+</html>
