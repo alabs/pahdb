822b7b0715150ffff94e1d89d3fae8bf29722115
Iván Eixarch <miao.org@gmail.com>
2013-03-19 05:18:23 +0100

    error sintaxis

--- app/prueba.php
+++ app/prueba.php
@@ -7,13 +7,15 @@ class Prueba {
 	function mostrar() {
 		$f3=$this->framework;
 		$f3->set('contenido','prueba.html');
-		echo Template::instance()->render('layout.htm');
 	}
 	function afectado() {
 		$f3=$this->framework;
 		$f3->set('idAfectado', $f3->get('GET.idAfectado'));
 		$f3->set('contenido','afectado.html');
-		echo Template::instance()->render('layout.htm');
+	}
+
+	function afterroute() {
+		echo Template::instance()->render('layout.html');
 	}
 
 	function __construct() {
