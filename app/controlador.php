5752d9c1893bfe6c87d87afd79779e2038a276b5
pahdb <pahdb@ksa.(none)>
2013-03-21 04:30:45 +0100

    probando gumby

---
+++ app/controlador.php
@@ -0,0 +1,72 @@
+<?php
+
+class Controlador {
+
+	protected
+		$framework,
+		$db;
+
+	//! HTTP route pre-processor
+	function beforeroute() {
+		$f3=$this->framework;
+		//Aqui quizás autenticación? se la pasan por el forro las clases heredadas O_o
+		$autorizado=$f3->get('SESSION.autorizado');
+$f3->set('prueba', 'tikitiki');
+		if (!$autorizado && $f3->get('URI')!='/entrar') {
+		//if (!$autorizado) {
+			//$f3->reroute('/entrar');
+			$this->autentificar();
+		} 
+	}
+
+	//! HTTP route post-processor
+	function afterroute() {
+		echo Template::instance()->render('layout.html');
+	}
+
+	function entrar() {
+		$f3=$this->framework;
+		$f3->set('contenido','login.html');
+	}
+
+	function salir() {
+		$f3=$this->framework;
+		$f3->clear('SESSION');
+		$f3->reroute('/entrar');
+	}
+
+	function autentificar() {
+		$f3=$this->framework;
+		$correo=$f3->get('POST.correo');
+		$passwd=$f3->get('POST.passwd');
+		if ($passwd == 'pepe') {
+			$f3->set('SESSION.correo', $correo);
+			$f3->set('SESSION.autorizado', true);
+			$f3->reroute($f3->get('URI'));
+			//$this->entrar();
+		} else {
+			$f3->set('aviso','Correo o contraseña incorrecta');
+			//$this->entrar();
+			$f3->reroute('/entrar');
+		}
+	}
+
+	//! Instantiate class
+	function __construct() {
+		$f3=Base::instance();
+		/*// Connect to the database
+		$db=new DB\SQL($f3->get('db'));
+		if (file_exists('setup.sql')) {
+			// Initialize database with default setup
+			$db->exec(explode(';',$f3->read('setup.sql')));
+			// Make default setup inaccessible
+			rename('setup.sql','setup.$ql');
+		}
+		// Use database-managed sessions
+		new DB\SQL\Session($db);
+		// Save frequently used variables
+		$this->db=$db;*/
+		$this->framework=$f3;
+	}
+
+}
