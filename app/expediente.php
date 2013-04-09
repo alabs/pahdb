a8bf295b6103b37c2655006eb430f78a21fdee4e
Iván Eixarch <ivan@alabs.org>
2013-04-09 21:18:01 +0200

    formulario de gestión de expedientes

--- app/expediente.php
+++ app/expediente.php
@@ -5,7 +5,6 @@ class Expediente extends Controlador {
 	function todos() {
 		$f3=$this->framework;
 		$bd=$this->bd;
-		//$afectados=new DB\SQL\Mapper($db,'Afectados');
 		$f3->set('afectados',
 			$bd->exec('SELECT idAfectado, nombre FROM Afectados')
 		);
@@ -14,12 +13,42 @@ class Expediente extends Controlador {
 
 	function editar() {
 		$f3=$this->framework;
-		$f3->set('idAfectado', $f3->get('PARAMS.idAfectado'));
+		$bd=$this->bd;
+		$afectado=new DB\SQL\Mapper($bd,'Afectados');
+		//$hipoteca=new DB\SQL\Mapper($bd,'Hipotecas');
+		if (!$f3->exists('PARAMS.idAfectado')) $f3->set('PARAMS.idAfectado','');
+		$afectado->load(array('idAfectado=?', $f3->get('PARAMS.idAfectado')));
+		//$hipoteca->load(array('idAfectado=?', $f3->get('PARAMS.idAfectado')));
+		$f3->set('HIPOTECAS', 
+			$bd->exec('SELECT * FROM Hipotecas WHERE idAfectado=?',$f3->get('PARAMS.idAfectado')));
+		//if (!$afectado->dry()) {
+			$afectado->copyto('AFECTADO');
+			//$hipoteca->copyto('HIPOTECA');
+		//}
 		$f3->set('contenido','form-afectados.html');
 	}
 	function guardar() {
 		$f3=$this->framework;
-		$f3->set('idAfectado', $f3->get('PARAMS.idAfectado'));
-		//$f3->set('contenido','afectado.html');
+		$bd=$this->bd;
+		$afectado=new DB\SQL\Mapper($bd,'Afectados');
+		$hipoteca=new DB\SQL\Mapper($bd,'Hipotecas');
+		$afectado->load(array('idAfectado=?', $f3->get('POST.idAfectado')));
+		$hipoteca->load(array('idAfectado=?', $f3->get('PARAMS.idAfectado')));
+		$afectado->copyfrom('POST');
+		//$hipoteca->copyfrom('POST');
+		$f3->set('PARAMS.idAfectado', $f3->get('POST.idAfectado'));
+		if ($afectado->dry()) {
+			// Nuevo afectado
+			//$hipoteca->save();
+			$afectado->save();
+		} else {
+			// Modificar afectado
+			//$hipoteca->update();
+			$afectado->update();
+		}
+
+		$this->editar();
+		//$afectado->copyto('AFECTADO');
+		//$f3->set('contenido','form-afectados.html');
 	}
 }
