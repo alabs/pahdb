5a88b97ca5bb4f3356c502ec884d8707f62ca274
Iván Eixarch <ivan@alabs.org>
2013-05-21 17:12:47 +0200

    solucionando grabación múltiples expedientes

--- app/expediente.php
+++ app/expediente.php
@@ -15,7 +15,7 @@ class Expediente extends Controlador {
 		$f3=$this->framework;
 		$bd=$this->bd;
 		$afectado=new DB\SQL\Mapper($bd,'Afectados');
-		//if (!$f3->exists('PARAMS.idAfectado')) $f3->set('PARAMS.idAfectado',0);
+		if (!$f3->exists('PARAMS.idAfectado')) $f3->set('PARAMS.idAfectado',NULL);
 		$afectado->load(array('idAfectado=?', $f3->get('PARAMS.idAfectado')));
 		//$hipoteca->load(array('idAfectado=?', $f3->get('PARAMS.idAfectado')));
 		//$f3->set('HIPOTECAS', 
@@ -36,7 +36,10 @@ class Expediente extends Controlador {
 		if ($afectado->dry()) {
 			// Nuevo afectado
 			//$hipoteca->save();
+			//$afectado->reset();
+			//$afectado->copyfrom('POST');
 			$afectado->save();
+			////$f3->set('PARAMS.idAfectado', $afectado->idAfectado);
 		} else {
 			// Modificar afectado
 			//$hipoteca->update();
